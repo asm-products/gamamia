ff5011cff0e0d091e8a6e2da3784ef2f6713e753
Philipp Wesner <pwesner@innomind.info>
2015-01-09 01:24:29 +0100

    added basic game submission form

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,23 @@
 class GamesController < ApplicationController
-	def index
-		@games = Game.all
-	end
+  def index
+    @games = Game.all
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to games_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:title, :thumbnail, :description, :status, :link, :platform)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'games#index'
+  resources :games, only: [:index, :new, :create]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
