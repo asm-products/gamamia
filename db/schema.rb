fd0d363cc63ef3afc0ad051580cb79a416e00b61
Philipp Wesner <pwesner@innomind.info>
2015-01-15 00:55:23 +0100

    cache votes and use them to display up and downvotes in games index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141219200437) do
+ActiveRecord::Schema.define(version: 20150114235123) do
 
   create_table "comments", force: true do |t|
     t.string   "content"
@@ -34,7 +34,22 @@ ActiveRecord::Schema.define(version: 20141219200437) do
     t.integer  "votes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "cached_votes_total",      default: 0
+    t.integer  "cached_votes_score",      default: 0
+    t.integer  "cached_votes_up",         default: 0
+    t.integer  "cached_votes_down",       default: 0
+    t.integer  "cached_weighted_score",   default: 0
+    t.integer  "cached_weighted_total",   default: 0
+    t.float    "cached_weighted_average", default: 0.0
   end
+
+  add_index "games", ["cached_votes_down"], name: "index_games_on_cached_votes_down"
+  add_index "games", ["cached_votes_score"], name: "index_games_on_cached_votes_score"
+  add_index "games", ["cached_votes_total"], name: "index_games_on_cached_votes_total"
+  add_index "games", ["cached_votes_up"], name: "index_games_on_cached_votes_up"
+  add_index "games", ["cached_weighted_average"], name: "index_games_on_cached_weighted_average"
+  add_index "games", ["cached_weighted_score"], name: "index_games_on_cached_weighted_score"
+  add_index "games", ["cached_weighted_total"], name: "index_games_on_cached_weighted_total"
 
   create_table "identities", force: true do |t|
     t.integer  "user_id"
@@ -61,6 +76,7 @@ ActiveRecord::Schema.define(version: 20141219200437) do
     t.datetime "updated_at"
     t.string   "name"
     t.string   "occupation"
+    t.boolean  "is_admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
