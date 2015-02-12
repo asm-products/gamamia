f6550c3db7171ddd4602da281783ee94a55095f4
Chris Lloyd <christopher.lloyd@gmail.com>
2015-02-12 12:12:05 -0800

    Fix tests.

--- spec/controllers/admin/games_controller_spec.rb
+++ spec/controllers/admin/games_controller_spec.rb
@@ -17,14 +17,6 @@ RSpec.describe Admin::GamesController do
     end
   end
 
-  describe "GET show" do
-    subject { get :show, {id: game, layout: 'admin'} }
-    it "renders admin/index template for admins" do
-      @user.update_attributes is_admin: true
-      expect(subject).to render_template('admin/games/show')
-    end
-  end
-
   describe "GET index" do
     subject { get :index, layout: 'admin' }
     it "renders admin/index template for admins" do
