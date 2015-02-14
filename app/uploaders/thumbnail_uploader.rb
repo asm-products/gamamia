608a790ea784fe631904aa510cc9ff0a03bea582
Chris Lloyd <christopher.lloyd@gmail.com>
2015-02-13 16:18:26 -0800

    Upload files to AWS-S3

--- app/uploaders/thumbnail_uploader.rb
+++ app/uploaders/thumbnail_uploader.rb
@@ -6,14 +6,6 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
-  def cache_dir
-    Rails.root.join('tmp', 'uploads')
-  end
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
