d265a18cc38f46096b3d8f78e47210712b24ee9b
Chris Lloyd <christopher.lloyd@gmail.com>
2015-02-13 17:02:33 -0800

    Fix game platforms having the wrong value.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,16 @@
 class Game < ActiveRecord::Base
 
   STATUS_TYPES = ["released", 0], ["beta",1]
-  PLATFORM_TYPES = ["PC", 0], ["Mac", 1], ["Linux", 2], ["iOS", 3], ["Android", 4], ["Windows Phone", 5]
+
+  PLATFORMS = [
+    'PC',
+    'Mac',
+    'Linux',
+    'iOS',
+    'Android',
+    'Windows Phone',
+    'Web'
+  ]
 
   has_many :videos
   has_many :comments
