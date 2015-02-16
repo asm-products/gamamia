7c723c21b0fa6a7655ea2218f99b305fbb252f89
Dusan Orlovic <duleorlovic@gmail.com>
2015-02-16 13:22:01 +0100

    Add some keys for twitter auth for new users can test

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -234,10 +234,10 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
-  config.omniauth :twitter, ENV['TWITTER_APP_ID'], ENV['TWITTER_APP_SECRET']
+  config.omniauth :twitter, Rails.application.secrets.twitter_app_id, Rails.application.secrets.twitter_app_secret 
 
   # ==> Warden configuration
-  # If you want to use other strategies, that are not supported by Devise, or
+  # if you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
   #
   # config.warden do |manager|
