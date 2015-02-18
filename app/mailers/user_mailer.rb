8031931a991b1f93b01e3135f77547decd2b0fd6
JasonHoffmann <jhoffm34@gmail.com>
2015-02-18 17:32:49 -0500

    Send emails only to non-admins

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   def game_scheduled(game)
     @user = game.user
     @game = game
-    # return if @user.is_admin?
+    return if @user.is_admin?
     mail(to: @user.email, subject: "Your game #{@game.title} has been accepted") do |format|
     	format.html { render layout: 'mailer.html.erb'}
     	format.text
