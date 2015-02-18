2ef12cf90c1dbeeab63a97d98dcb648cecd2bcab
Philipp Wesner <pwesner@innomind.info>
2015-02-18 20:37:50 +0100

    fixed current_ability

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,7 +1,8 @@
 class Admin::ApplicationController < ApplicationController
   def current_ability
     if @current_ability.nil? || @current_ability.class != AdminAbility
-      @current_ability || AdminAbility.new(current_user)
+      @current_ability = AdminAbility.new(current_user)
     end
+    @current_ability
   end
 end
