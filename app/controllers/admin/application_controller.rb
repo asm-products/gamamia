5e00c43dfcba08595fcce879a5ffa1610269cffe
JasonHoffmann <jhoffm34@gmail.com>
2015-02-24 09:32:51 -0500

    Adding admin filter

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ApplicationController < ApplicationController
+  before_filter :current_ability
+  
   def current_ability
     if @current_ability.nil? || @current_ability.class != AdminAbility
       @current_ability = AdminAbility.new(current_user)
