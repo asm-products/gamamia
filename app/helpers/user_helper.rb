0adec1284f989be871064fc304fa296f5cfc081c
JasonHoffmann <jhoffm34@gmail.com>
2015-02-25 13:30:20 -0500

    Extending avatar helper

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,15 +1,23 @@
 module UserHelper
-  def avatar_for(user)
+  def avatar_for(user, size)
     return unless user
     avatar_url = if user.avatar_url.nil?
       avatar_path = Digest::MD5.hexdigest(user.email.downcase)
-      "https://gravatar.com/avatar/#{avatar_path}.png?s=48&d=monsterid"
+      if size == 'small'
+        "https://gravatar.com/avatar/#{avatar_path}.png?s=48&d=monsterid"
+      elsif size == 'large'
+        "https://gravatar.com/avatar/#{avatar_path}.png?s=130&d=monsterid"
+      end
     else
       user.avatar_url
     end
 
     link_to "#", class: "block" do
-      image_tag avatar_url, class: "block", width: "24", height: "24"
+      if size == 'small'
+        image_tag avatar_url, class: "block", width: "24", height: "24"
+      elsif size == 'large'
+        image_tag avatar_url, class: "block", width: "130", height: "130"
+      end
     end
   end
 end
