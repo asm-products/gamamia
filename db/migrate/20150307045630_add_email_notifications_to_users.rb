e66934c6ba3ecbdfe129ebd810c2ac561547b397
Christopher Lai <christopher@herelde.com>
2015-03-06 21:17:41 -0800

    User#email_notifications added

---
+++ db/migrate/20150307045630_add_email_notifications_to_users.rb
@@ -0,0 +1,5 @@
+class AddEmailNotificationsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :email_notifications, :boolean, default: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226153242) do
+ActiveRecord::Schema.define(version: 20150307045630) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -19,10 +19,6 @@ ActiveRecord::Schema.define(version: 20150226153242) do
     t.integer  "game_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "parent_id"
-    t.integer  "lft"
-    t.integer  "rgt"
-    t.integer  "children_count"
     t.text     "cached_content"
   end
 
@@ -92,12 +88,12 @@ ActiveRecord::Schema.define(version: 20150226153242) do
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",   null: false
+    t.string   "encrypted_password",     default: "",   null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,    null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -108,9 +104,10 @@ ActiveRecord::Schema.define(version: 20150226153242) do
     t.string   "occupation"
     t.text     "avatar_url"
     t.boolean  "receive_newsletter"
-    t.string   "username",               default: "", null: false
+    t.string   "username",               default: "",   null: false
     t.string   "role"
     t.string   "twitter_handle"
+    t.boolean  "email_notifications",    default: true
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
