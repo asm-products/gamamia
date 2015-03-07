d4236541e8434fc623a514962a31b411ffe02020
Christopher Lai <christopher@herelde.com>
2015-03-06 21:42:17 -0800

    Mention emails no sent if User#email_notifications is false

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,8 +5,16 @@ RSpec.describe Comment do
   let(:user) { Fabricate :user }
   let(:mention_comment){ Fabricate :comment, user: user, content: "Hello @#{user.username}. nice to meet you" }
 
+  before { ActionMailer::Base.deliveries = [] }
+
   it "should deliver email" do
     expect{ mention_comment }.to change { ActionMailer::Base.deliveries.count }.by(1)
+  end
+
+  it "should not deliver email if User#email_notifications is false" do
+    user.update(email_notifications: false)
+
+    expect{ mention_comment }.to_not change{ ActionMailer::Base.deliveries.count }.from(0)
   end
 
   it "should cache html output" do
