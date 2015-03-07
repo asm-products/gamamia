a582241e66a983bba68dfcd91b15163b3c3e11a8
JasonHoffmann <jhoffm34@gmail.com>
2015-03-07 10:41:53 -0500

    Resetting schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307045630) do
+ActiveRecord::Schema.define(version: 20150227142334) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20150307045630) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "parent_id"
-    t.text     "cached_content"
   end
 
   add_index "comments", ["game_id"], name: "index_comments_on_game_id"
@@ -90,12 +89,12 @@ ActiveRecord::Schema.define(version: 20150307045630) do
   add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",   null: false
-    t.string   "encrypted_password",     default: "",   null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,    null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -106,10 +105,9 @@ ActiveRecord::Schema.define(version: 20150307045630) do
     t.string   "occupation"
     t.text     "avatar_url"
     t.boolean  "receive_newsletter"
-    t.string   "username",               default: "",   null: false
+    t.string   "username",               default: "", null: false
     t.string   "role"
     t.string   "twitter_handle"
-    t.boolean  "email_notifications",    default: true
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
