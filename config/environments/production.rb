77fea708c61c7a7cec72141550925c35a01c256d
JasonHoffmann <jhoffm34@gmail.com>
2015-03-13 10:24:02 -0400

    Adding host to mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'gamamia.herokuapp.com' }
 end
 
 ActionMailer::Base.smtp_settings = {
