815eee8b251c2246bfb52fbce2017ced77ec07c0
JasonHoffmann <jhoffm34@gmail.com>
2015-04-13 16:22:47 -0400

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -26,9 +26,9 @@ ActiveRecord::Schema.define(version: 20150317210255) do
     t.integer  "parent_id"
   end
 
-  add_index "comments", ["game_id"], name: "index_comments_on_game_id"
-  add_index "comments", ["parent_id"], name: "index_comments_on_parent_id"
-  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+  add_index "comments", ["game_id"], name: "index_comments_on_game_id", using: :btree
+  add_index "comments", ["parent_id"], name: "index_comments_on_parent_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "games", force: :cascade do |t|
     t.string   "title",                                 null: false
@@ -53,14 +53,14 @@ ActiveRecord::Schema.define(version: 20150317210255) do
     t.integer  "user_id"
   end
 
-  add_index "games", ["cached_votes_down"], name: "index_games_on_cached_votes_down"
-  add_index "games", ["cached_votes_score"], name: "index_games_on_cached_votes_score"
-  add_index "games", ["cached_votes_total"], name: "index_games_on_cached_votes_total"
-  add_index "games", ["cached_votes_up"], name: "index_games_on_cached_votes_up"
-  add_index "games", ["cached_weighted_average"], name: "index_games_on_cached_weighted_average"
-  add_index "games", ["cached_weighted_score"], name: "index_games_on_cached_weighted_score"
-  add_index "games", ["cached_weighted_total"], name: "index_games_on_cached_weighted_total"
-  add_index "games", ["user_id"], name: "index_games_on_user_id"
+  add_index "games", ["cached_votes_down"], name: "index_games_on_cached_votes_down", using: :btree
+  add_index "games", ["cached_votes_score"], name: "index_games_on_cached_votes_score", using: :btree
+  add_index "games", ["cached_votes_total"], name: "index_games_on_cached_votes_total", using: :btree
+  add_index "games", ["cached_votes_up"], name: "index_games_on_cached_votes_up", using: :btree
+  add_index "games", ["cached_weighted_average"], name: "index_games_on_cached_weighted_average", using: :btree
+  add_index "games", ["cached_weighted_score"], name: "index_games_on_cached_weighted_score", using: :btree
+  add_index "games", ["cached_weighted_total"], name: "index_games_on_cached_weighted_total", using: :btree
+  add_index "games", ["user_id"], name: "index_games_on_user_id", using: :btree
 
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 20150317210255) do
     t.datetime "updated_at"
   end
 
-  add_index "identities", ["user_id"], name: "index_identities_on_user_id"
+  add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
 
   create_table "taggings", force: :cascade do |t|
     t.integer  "tag_id"
@@ -82,15 +82,15 @@ ActiveRecord::Schema.define(version: 20150317210255) do
     t.datetime "created_at"
   end
 
-  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
 
   create_table "tags", force: :cascade do |t|
     t.string  "name"
     t.integer "taggings_count", default: 0
   end
 
-  add_index "tags", ["name"], name: "index_tags_on_name", unique: true
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",   null: false
@@ -115,9 +115,9 @@ ActiveRecord::Schema.define(version: 20150317210255) do
     t.boolean  "email_notifications",    default: true
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   create_table "videos", force: :cascade do |t|
     t.string   "title"
@@ -129,7 +129,7 @@ ActiveRecord::Schema.define(version: 20150317210255) do
     t.datetime "updated_at"
   end
 
-  add_index "videos", ["game_id"], name: "index_videos_on_game_id"
+  add_index "videos", ["game_id"], name: "index_videos_on_game_id", using: :btree
 
   create_table "votes", force: :cascade do |t|
     t.integer  "votable_id"
@@ -143,12 +143,11 @@ ActiveRecord::Schema.define(version: 20150317210255) do
     t.datetime "updated_at"
   end
 
-  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
-  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
+  add_index "votes", ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope", using: :btree
+  add_index "votes", ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope", using: :btree
 
   add_foreign_key "comments", "games", name: "comments_game_id_fk"
   add_foreign_key "comments", "users", name: "comments_user_id_fk"
-  add_foreign_key "games", "users"
   add_foreign_key "games", "users", name: "games_user_id_fk"
   add_foreign_key "identities", "users", name: "identities_user_id_fk"
   add_foreign_key "videos", "games", name: "videos_game_id_fk"
